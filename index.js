8d70cd20d76a877d53fa81b463b309f13b0b7339
Kyle <360klacy@gmail.com>
2019-07-22 15:44:44 -0400

    Fixed missing JS

--- index.js
+++ index.js
@@ -89,14 +89,3 @@ use the existing "avatar" code to add employees to the screen
 2)  Add a filter to the data to only return users whose last
     name start with a given letter
 */
-/*
-
-We are going to create an Employee address book.  We will
-use the existing "avatar" code to add employees to the screen
-
-1)  Update the JavaScript code to read all of the user data from
-    the people.js file and show all the users on the page
-2)  Add a filter to the data to only return users whose last
-    name start with a given letter
-
-*/
